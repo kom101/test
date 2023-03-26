7711fdfe235a81ed509d80e537bec1da10061284
kom101 <kom101@wp.pl>
2023-03-26 20:00:40 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -10,7 +10,12 @@ def deployApp() {
     echo 'deplying the application...'
     echo "deploying version ${params.VERSION}"
     def files = findFiles(glob: '**/*.*', excludes: 'b/*.txt,**/aba/*.txt')
-    echo """${files[0].name} ${files[0].path} ${files[0].directory} ${files[0].length} ${files[0].lastModified}""" 
+    for file in files:
+      echo """
+      ${files[0].name} ${files[0].path} ${files[0].directory} 
+      ${files[0].length} ${files[0].lastModified}
+      """
+
 } 
 
 return this
