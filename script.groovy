c65fad4854c50e99631954bfe35c419914faa482
kom101 <kom101@wp.pl>
2023-03-26 19:57:40 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -9,6 +9,8 @@ def testApp() {
 def deployApp() {
     echo 'deplying the application...'
     echo "deploying version ${params.VERSION}"
+    def files = findFiles(glob: '**/*.*', excludes: 'b/*.txt,**/aba/*.txt')
+    echo """${files[0].name} ${files[0].path} ${files[0].directory} ${files[0].length} ${files[0].lastModified}""" 
 } 
 
 return this
